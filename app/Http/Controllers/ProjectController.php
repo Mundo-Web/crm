f79d30650fe0506640164b8129ca2ef4b87488dd
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-04 14:14:57 -0500

    NTS

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -38,7 +38,7 @@ class ProjectController extends BasicController
             DB::raw('MAX(payments.created_at) AS last_payment_date'),
         ])
             ->leftJoin('payments', 'payments.project_id', 'projects.id')
-            ->groupBy('projects.id', 'type.id');
+            ->groupBy('projects.id', 'type_id');
     }
 
     static function projectStatus(Request $request)
