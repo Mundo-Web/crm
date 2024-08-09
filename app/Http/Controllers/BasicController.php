2ef7c388f179890c40486fa9ee62062f9709b583
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-09 09:32:54 -0500

    Hola mundo

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -127,11 +127,7 @@ class BasicController extends Controller
 
       $totalCount = 0;
       if ($request->requireTotalCount) {
-        if ($this->prefix4filter) {
-          $totalCount = $instance->count("{$this->prefix4filter}.*");
-        } else {
-          $totalCount = $instance->count("*");
-        }
+        $totalCount = $instance->count();
       }
 
       $jpas = $request->isLoadingAll
