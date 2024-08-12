bb710a0b0b5fe020e329e9fe714d578782cd50e0
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-12 12:57:35 -0500

    Quitando espacios signos, y agregando nombre de creador en la busqueda de lead

--- resources/js/Reutilizables/ClientNotes/ClientNotesCard.jsx
+++ resources/js/Reutilizables/ClientNotes/ClientNotesCard.jsx
@@ -4,8 +4,8 @@ import TaskCard from "../Tasks/TaskCard"
 
 const ClientNotesCard = ({ type, name, description, created_at, tasks, onTaskChange }) => {
   if (!type) {
-    return <div className="card card-body">
-      <p className="card-text">
+    return <div className="card card-body" style={{ border: '1px solid #98a6ad' }}>
+      <p className="card-text mb-0">
         <i className='mdi mdi-clock me-1'></i>
         {name}
       </p>
