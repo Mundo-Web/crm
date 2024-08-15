25ad46abe629f2ff6eb3244a30f2e9c1dac53f2f
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-15 03:34:46 -0500

    Building

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -305,7 +305,7 @@ class LeadController extends BasicController
 
             $response->message = 'Se ha creado el lead correctamente';
         });
-
+        dump($response->toArray());
         return response($response->toArray(), $response->status);
     }
 }
