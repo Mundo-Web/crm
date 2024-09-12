e62285c311079a6482b78017930d5bbb669ab0a3
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-09-12 17:04:32 -0500

    NTS

--- routes/free.php
+++ routes/free.php
@@ -10,6 +10,6 @@ Route::get('/remainings-history', [RemainingHistoryController::class, 'set']);
 Route::post('/clients', [ClientController::class, 'save']);
 Route::post('/leads', [LeadController::class, 'external']);
 
-Route::get('/messages/{session_id}', [MessageController::class, 'byPhone']);
+Route::post('/messages/{session_id}', [MessageController::class, 'byPhone']);
 Route::post('/messages', [MessageController::class, 'save']);
 Route::post('/messages/help', [MessageController::class, 'help']);
