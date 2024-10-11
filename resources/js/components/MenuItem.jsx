1e6e26af857edd8864635e5d9df10edc03a90d27
sodeworld <160994235+sodeworld@users.noreply.github.com>
2024-10-11 14:41:57 -0500

    Correccion de estados y agregando dashboards de leads

--- resources/js/components/MenuItem.jsx
+++ resources/js/components/MenuItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MenuItem = ({id, href, icon, children, badge }) => {
   return (
-    <li className={location.pathname.startsWith(href) ? 'menuitem-active' : ''}>
+    <li className={location.pathname == href ? 'menuitem-active' : ''}>
       <a id={id} href={href} className={location.pathname == href ? 'active' : ''}>
         <i className={icon}></i>
         {
