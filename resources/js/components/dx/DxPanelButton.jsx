49bab532b3fd9878ec452770c864492fd2129e98
sodeworld <160994235+sodeworld@users.noreply.github.com>
2024-11-06 11:07:05 -0500

    Quitando texto en mobile

--- resources/js/components/dx/DxPanelButton.jsx
+++ resources/js/components/dx/DxPanelButton.jsx
@@ -13,8 +13,8 @@ const DxPanelButton = ({ className = 'btn btn-xs btn-default rounded-pill', loca
         container.parent().attr('style', 'border: none !important; background: transparent !important')
         container.attr('class', className)
         container.html(renderToString(<>
-          {data.icon && <i className={`${data.icon} me-1`}></i>}
-          <span className="d-none d-md-block">{data.text}</span>
+          {data.icon && <i className={`${data.icon}`}></i>}
+          <span className="d-none d-md-block ms-1">{data.text}</span>
         </>))
       }
     }
