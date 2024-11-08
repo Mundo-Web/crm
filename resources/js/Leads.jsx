6415bd068a77f595afe008a15ac8a47cf7d49ffb
sodeworld <160994235+sodeworld@users.noreply.github.com>
2024-11-08 17:12:52 -0500

    Hola mundo

--- resources/js/Leads.jsx
+++ resources/js/Leads.jsx
@@ -1107,7 +1107,7 @@ const Leads = ({ statuses: statusesFromDB, defaultClientStatus, defaultLeadStatu
                           <div class="card-header p-2">
                             <b className='d-block'>
                               <i class="mdi mdi-email me-1"></i>
-                              <HtmlContent className={'d-inline-block'} html={sender}/>
+                              <HtmlContent className={'d-inline'} html={sender}/>
                             </b>
                             <small className='text-muted'>{moment(date).format('LLL')}</small>
                           </div>
