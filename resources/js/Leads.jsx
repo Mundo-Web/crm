48b9e910f6866a90d79457c35bb9a8bc2ccfdcd2
sodeworld <160994235+sodeworld@users.noreply.github.com>
2024-11-08 17:26:54 -0500

    Hola mundo

--- resources/js/Leads.jsx
+++ resources/js/Leads.jsx
@@ -1101,9 +1101,13 @@ const Leads = ({ statuses: statusesFromDB, defaultClientStatus, defaultLeadStatu
                         const sender = String(mail.sender).replace(/\<(.*?)\>/g, '<span class="me-1">·</span><small style="font-weight: lighter">&lt;$1&gt;</small>')
                         const date = new Date(mail.date)
                         return <div key={index} className="card mb-2" style={{ border: '1px solid rgb(222, 226, 230)' }}>
-                          <div class="card-header p-2" style={{ backgroundColor: mail.type == 'sent' ? '#f2f6fc': '#ffffff'}}>
+                          <div class="card-header p-2">
                             <b className='d-block'>
-                              <i class="mdi mdi-email me-1"></i>
+                              {
+                                mail.type == 'sent'
+                                  ? <i class="mdi mdi-inbox me-1"></i>
+                                  : <i className='mdi mdi-send me-1'></i>
+                              }
                               <HtmlContent className={'d-inline'} html={sender} />
                             </b>
                             <small className='text-muted'>{moment(date).format('LLL')}</small>
