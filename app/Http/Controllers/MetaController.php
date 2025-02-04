4ad4287c3935abc087b01c5253c2c3278b65e658
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-02-04 12:50:04 -0500

    Agregando webhook

--- app/Http/Controllers/MetaController.php
+++ app/Http/Controllers/MetaController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class MetaController extends Controller
 {
     public function verify(Request $request) {
-        $challenge = $request->query('hub.challenge');
+        $challenge = $request->query('hub_challenge');
         dump($request->all());
         return \response($challenge, 200);
     }
