67dcddcd13d31ff7b15ee96fd568cfa300226c56
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-03-13 18:19:32 -0500

    aAgregando firma

--- resources/js/components/OffCanvas.jsx
+++ resources/js/components/OffCanvas.jsx
@@ -138,11 +138,13 @@ const OffCanvas = ({ offCanvasRef, dataLoaded, setDataLoaded }) => {
                     {/* <span className="user-name">{message.role == 'Human' ? dataLoaded?.contact_name : ''}</span> */}
                     {
                       content.startsWith('/signature:')
-                        ? <img src={content.replace('/signature:', '')} alt={`${LaravelSession.service_user.fullname} signature`} style={{
-                          maxWidth: '100%',
-                          maxHeight: '200px',
-                          borderRadius: '4px'
-                        }} />
+                        ? <img src={content.replace('/signature:', '')} alt={`${LaravelSession.service_user.fullname} signature`}
+                          style={{
+                            maxWidth: '100%',
+                            maxHeight: '200px',
+                            borderRadius: '4px'
+                          }}
+                        />
                         : { content }
                     }
                     {/* <p>
