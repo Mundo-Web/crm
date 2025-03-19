daff6b010528b2fb074ba3a4e04387de0324859a
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-03-19 02:14:14 -0500

    Filtrar solo los mensajes por defecto de los usuarios, no mostrar todos de la empresa

--- app/Http/Controllers/DefaultMessageController.php
+++ app/Http/Controllers/DefaultMessageController.php
@@ -11,6 +11,10 @@ class DefaultMessageController extends BasicController
     public $model = DefaultMessage::class;
     public $reactView = 'DefaultMessages';
 
+    function setPaginationInstance(Request $request, string $model)
+    {
+        return $model::where('user_id', Auth::id());
+    }
     public function beforeSave(Request $request)
     {
         $body = $request->all();
