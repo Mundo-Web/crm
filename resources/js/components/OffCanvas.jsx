30339e1d703e2e7b809202dfc6251fcae10d2eb1
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-03-24 17:11:31 -0500

    Mostrando el mensaje de whatsapp como Html

--- resources/js/components/OffCanvas.jsx
+++ resources/js/components/OffCanvas.jsx
@@ -281,7 +281,7 @@ const OffCanvas = ({ offCanvasRef, dataLoaded, setDataLoaded, defaultMessages, s
                               </>
                             }
                             {/* <p>{content.replace(attachment, '')}</p> */}
-                            <p>{wa2html(content.replace(attachment, ''))}</p>
+                            <HtmlContent html={wa2html(content.replace(attachment, ''))} />
                           </>
                       }
                     </div>
