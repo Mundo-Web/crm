16586eec3bd7834a13914f83b2a82ea653130f19
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-05-08 11:26:02 -0500

    NTS

--- app/Http/Controllers/MetaController.php
+++ app/Http/Controllers/MetaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Atalaya\ServicesByBusiness;
 use Exception;
 use Illuminate\Http\Request;
+use SoDe\Extend\Fetch;
 use SoDe\Extend\JSON;
 use SoDe\Extend\Response;
 
@@ -15,10 +16,7 @@ class MetaController extends Controller
         $challenge = $request->query('hub_challenge');
         $verify_token = $request->query('hub_verify_token');
 
-        if (!in_array($origin, ['messenger', 'instagram'])) {
-            dump('Origen no permitido: ' . $origin);
-            return response('Error, origen no permitido', 403);
-        }
+        if (!in_array($origin, ['messenger', 'instagram'])) return response('Error, origen no permitido', 403);
 
         $businessExists = ServicesByBusiness::query()
             ->join('businesses', 'services_by_businesses.business_id', '=', 'businesses.id')
@@ -27,15 +25,9 @@ class MetaController extends Controller
             ->where('businesses.uuid', $business_id)
             ->where('businesses.status', true)
             ->exists();
-        if (!$businessExists) {
-            dump('Negocio no encontrado o inactivo: '. $business_id);
-            return response('Error, negocio no encontrado o inactivo', 403);
-        }
+        if (!$businessExists) return response('Error, negocio no encontrado o inactivo', 403);
 
-        if (hash('sha256', $business_id) != $verify_token) {
-            dump('Token de validación incorrecto: '. $verify_token);
-            return response('Error, token de validación incorrecto', 403);
-        }
+        if (hash('sha256', $business_id) != $verify_token) return response('Error, token de validación incorrecto', 403);
 
         return response($challenge, 200);
     }
@@ -47,17 +39,19 @@ class MetaController extends Controller
 
             $data = $request->all();
             $entry = $data['entry'] ?? [];
-            dump("{$origin} ({$business_id}): " . JSON::stringify($entry, true));
-            switch ($origin) {
-                case 'messenger':
-                    if ($entry['id'] != $entry['messaging']['sender']['id']) {
-                        dump($entry['messaging']['message']['text']);
-                    }
-                    break;
 
-                default:
-                    # code...
-                    break;
+            if ($entry['id'] == $entry['messaging']['recipient']['id']) return;
+
+            $userId = $entry['messaging']['sender']['id'];
+            $fields = ['id', 'first_name', 'last_name', 'name', 'profile_pic', 'locale', 'timezone', 'gender'];
+            $accessToken = 'EAATRvPtpfZAMBO8FM65hJK1Vw1NAlZADDJvSzvWZAsqd4hgPwJMohfaCRFG7oD3ZBhpJZCWUTABWjaqJwUKgymaqYIluYJ9fEyk41O6UZAptOYozu5l58hz6A9Nmpb1qEEZBndqpZBTGukTvf6qV0giz5ViZCLaNoJtipvIyOdFLOGZAvk3vRvk8ZAUquFEbAj4Xtmq';
+            $profileRest = new Fetch(env('META_GRAPH_URL') . "/{$userId}?fields={$fields}&access_token={$accessToken}");
+            $profileData = $profileRest->json();
+
+            dump($profileData);
+
+            if ($entry['id'] != $entry['messaging']['sender']['id']) {
+                dump($entry['messaging']['message']['text']);
             }
         });
         return response($response->toArray(), 200);
