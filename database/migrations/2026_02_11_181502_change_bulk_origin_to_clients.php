cc38473b5d525be3bcb7fbed6ce6fd5432b296f5
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2026-02-11 18:15:57 -0500

    Agrgeando cositas

---
+++ database/migrations/2026_02_11_181502_change_bulk_origin_to_clients.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('clients')
+            ->where('origin', 'Pauta')
+            ->update(['origin' => 'Google']);
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            //
+        });
+    }
+};
